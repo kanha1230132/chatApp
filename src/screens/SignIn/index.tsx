9ce8a9711e0e84b149c48569ce288c01c6e40d9e
Kanhaiya Lal <kanhaiyalalverma686@gmail.com>
2024-04-22 22:29:06 +0530

    Implement Signup screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AppColor from '../../theme/AppColor'
+import AppFonts from '../../theme/AppFonts'
 
 const SignInScreen = () => {
   return (
-    <View>
-      <Text>SignInScreen</Text>
-    </View>
+    <SafeAreaView
+    style={{
+      flex: 1,
+      backgroundColor: AppColor.WHITE,
+    }}>
+
+<View
+        style={{
+          marginTop: 50,
+          marginLeft: '5%',
+        }}>
+        <Text
+          style={{
+            fontSize: 25,
+            color: AppColor.WHITE,
+            fontFamily: AppFonts.REGULAR,
+            letterSpacing: 1.5,
+          }}>
+          Sign Up
+        </Text>
+        <Text
+          style={{
+            fontSize: 25,
+            color: AppColor.WHITE,
+            fontFamily: AppFonts.MEDIUM,
+            letterSpacing: 1.5,
+            marginTop: 10,
+          }}>
+          To
+        </Text>
+        <Text
+          style={{
+            fontSize: 25,
+            color: AppColor.WHITE,
+            fontFamily: AppFonts.BOLD,
+            letterSpacing: 1.5,
+            marginTop: 10,
+          }}>
+          ChatApp
+        </Text>
+      </View>
+
+      </SafeAreaView>
   )
 }
 
